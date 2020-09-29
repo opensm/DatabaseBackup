00177bfb007666ebd58e975af310379b53603c29
yaoshaoqiang <yaoshaoqiang@zhiyuanhui.com>
2020-09-29 14:39:50 +0800

    .gitignore添加

--- main.py
+++ main.py
@@ -8,6 +8,6 @@ p.pg_basedump(db_config='nccc', params="-Ft  -z -R -P ")
 
 
 def useage():
-    print("%s -h\t#帮助文档" % sys.argv[0])
+    print("%s -h \t#帮助文档" % sys.argv[0])
     print("%s -b #相关的数据库配置 \t#导入hive端启动" % sys.argv[0])
     print("%s -r #相关同步配置 \t#kafka消费端启动" % sys.argv[0])
