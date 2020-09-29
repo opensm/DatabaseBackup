84a9dd8e02a4a64d9ecf8812508a083f85959d4c
yaoshaoqiang <yaoshaoqiang@zhiyuanhui.com>
2020-09-29 14:57:49 +0800

    .gitignore添加

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ from lib.setting import BACKUP_PARAMS, BASE_BACKUP_PARAMS
 
 def useage():
     print("%s -h \t#帮助文档" % sys.argv[0])
-    print("%s -b #相关的数据库配置 \t#导入hive端启动" % sys.argv[0])
-    print("%s -r #相关同步配置 \t#kafka消费端启动" % sys.argv[0])
+    print("%s -b #备份postgres的配置 \t#备份postgres数据" % sys.argv[0])
+    print("%s -r [相关同步配置] -p [推送匹配字符，绝对路径] \t#手动推送文件" % sys.argv[0])
 
 
 def main():
@@ -25,18 +25,19 @@ def main():
         sys.exit(1)
     command_dict = dict(options)
     # 帮助
+    p = PostgresDumps()
     if command_dict.keys() == ['-h']:
         useage()
         sys.exit()
     # 获取监控项数据
     elif command_dict.keys() == ['-b']:
-        p = PostgresDumps()
         config_key = command_dict.get("-b")
         p.postgres_dump(db_config=config_key, params=BACKUP_PARAMS)
         p.pg_basedump(db_config=config_key, params=BASE_BACKUP_PARAMS)
     elif Counter(command_dict.keys()) == ['-r', '-p']:
-
-
+        rsync_cfg = command_dict.get("-r")
+        compare = command_dict.get("-p")
+        p.rsync_file(rsync_conf=rsync_cfg, compare=compare)
     else:
         useage()
         sys.exit(1)
